e7e59a78615a3f0c90b8eda5c343674a6934284c
John Murray <me@johnmurray.io>
2018-05-10 19:46:26 -0700

    basic site-generation support

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
-# Compiler / Linker Flags
-LD_FLAGS = LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
-CXX_FLAGS = CPPFLAGS="-I/usr/local/opt/llvm/include"
-CLANG = /usr/local/opt/llvm/bin/clang++
+CXX = g++-8
+CXX_LINK_FLAGS = -l ctemplate_nothreads -l boost_system -l boost_filesystem
 
-default: clean compile generate
+default: clean generate
 
 .PHONY: clean
 clean:
@@ -12,12 +10,16 @@ clean:
 .PHONY: compile
 compile:
 	mkdir -p build/bin/
-	$(LD_FLAGS) $(CXX_FLAGS) $(CLANG) -o build/bin/builder gen/main.cpp -lctemplate_nothreads -std=c++17
+	$(CXX) -o build/bin/builder gen/main.cpp $(CXX_LINK_FLAGS) -std=c++17
+
+.PHONY: generate-setup
+generate-setup:
+	@mkdir -p build/site/style
+	@mkdir -p build/site/assets
 
 .PHONY: generate
-generate:
+generate: generate-setup compile
 	@echo 'Generating Site...'
-	@mkdir -p build/site/assets
-	@cp site/index.html build/site/index.html
 	@lessc site/less/app.less build/site/style/app.css
 	@cp logo/logo.png build/site/assets/logo.png
+	@build/bin/builder site
--- gen/main.cpp
+++ gen/main.cpp
@@ -1,10 +1,41 @@
+#include <boost/filesystem.hpp>
 #include <cstdlib>
-#include <string>
-#include <iostream>
 #include <ctemplate/template.h>
-#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <streambuf>
+#include <string>
+
+namespace fs = boost::filesystem;
 
 int main(int argc, char** argv) {
-    ctemplate::TemplateDictionary dict("headers");
+    if (argc <= 1) {
+        std::cout << "[Error]: Must specify a site directory\n";
+        return 1;
+    }
+    auto site_dir = fs::path(std::string(argv[1]));
+    auto build_dir = (site_dir / ".." / "build" / "site");
+
+    ctemplate::TemplateDictionary template_dict("template_dict");
+
+    // Read includes into our template dictionary object
+    for (auto const &dir_entry: fs::recursive_directory_iterator(site_dir / "includes")) {
+        if (fs::is_regular_file(dir_entry)) {
+            auto filename = std::string(dir_entry.path().filename().c_str());
+            auto template_name = filename.substr(0, filename.find(".tpl.html"));
+
+            std::ifstream t(dir_entry.path().c_str());
+            std::string file_str((std::istreambuf_iterator<char>(t)),
+                 std::istreambuf_iterator<char>());
+
+            template_dict.SetValue(template_name, file_str);
+        }
+    }
+
+    std::string output;
+    ctemplate::ExpandTemplate((site_dir / "index.tpl.html").c_str(), ctemplate::DO_NOT_STRIP, &template_dict, &output);
+    std::ofstream index_out((build_dir / "index.html").c_str());
+    index_out << output;
+    index_out.close();
     return 0;
 }
