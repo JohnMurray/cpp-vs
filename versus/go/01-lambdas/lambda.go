6a06ef201f2f52ab2fef7745a95cfec1269006e5
John Murray <me@johnmurray.io>
2018-08-12 12:33:47 -0400

    Resolves #18 - C++ vs Go - Simple Web Server
    Added a simple web-server example using the c++ code from the Javascript
    Example and using an example I found online for using the stdlib in Go
    to create a web-server.

--- versus/go/01-lambdas/lambda.go
+++ versus/go/01-lambdas/lambda.go
@@ -1,15 +1,15 @@
 package main
 
 func main() {
-    addOne := func(x int) int {
-        return x + 1
-    }
+	addOne := func(x int) int {
+		return x + 1
+	}
 
-    addOne(5)
-    // returns 6
+	addOne(5)
+	// returns 6
 
-    // use lambda as a value
-    v := []int{1, 2, 3, 4, 5}
-    apply([]int{1, 2, 3, 4, 5}, addOne)
-    // new value of 'v' is [2, 3, 4, 5, 6]
+	// use lambda as a value
+	v := []int{1, 2, 3, 4, 5}
+	apply([]int{1, 2, 3, 4, 5}, addOne)
+	// new value of 'v' is [2, 3, 4, 5, 6]
 }
